ce2bb689163a1ed445bc280b219d2694e86ce03b
Amar <amar2@hpe.com>
2021-03-22 11:40:47 +0530

    Correcting the port details for consumer and producer

--- install/Docker/dockerfiles/scripts/start_producer.sh
+++ install/Docker/dockerfiles/scripts/start_producer.sh
@@ -42,7 +42,7 @@ start_producer()
 {
 	registry_address="consul:8500"
 	export CONFIG_FILE_PATH=/etc/odimra_config/odimra_config.json
-	nohup /bin/svc-producer --registry=consul --registry_address=${registry_address} --server_address=producer:45101 --client_request_timeout=`expr $(cat $CONFIG_FILE_PATH | grep SouthBoundRequestTimeoutInSecs | cut -d : -f2 | cut -d , -f1 | tr -d " ")`s >> /var/log/odimra_logs/producer.log 2>&1 &
+	nohup /bin/svc-producer --registry=consul --registry_address=${registry_address} --server_address=producer:45110 --client_request_timeout=`expr $(cat $CONFIG_FILE_PATH | grep SouthBoundRequestTimeoutInSecs | cut -d : -f2 | cut -d , -f1 | tr -d " ")`s >> /var/log/odimra_logs/producer.log 2>&1 &
 	PID=$!
 	sleep 3
 
